4d45ae95cd96224fecbc74b07f161adfc4c92a57
MilesHewitt <68699731+MilesHewitt@users.noreply.github.com>
2020-12-11 14:25:25 +0000

    Create Plot_Delta_Functions.py

---
+++ set-up-files/Plot_Delta_Functions.py
@@ -0,0 +1,19 @@
+# Imports
+from __future__ import print_function
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge, Lasso, SGDRegressor, ElasticNet, LinearRegression
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import math
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, cross_val_predict, cross_validate
+from sklearn.kernel_ridge import KernelRidge
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+from sklearn.ensemble import AdaBoostRegressor
+from numpy import genfromtxt
+
+#######################################################################################################################################
+# Find x and y errors for each regression model (in this case randomised
